cce0da1aa6e6fe7e11ad846a5f1a96b6abf6ada8
Chris Roerig <nothanks@localhost>
2026-01-18 15:29:04 -0600

    Update homegit to v1.0.2

--- Formula/homegit.rb
+++ Formula/homegit.rb
@@ -1,8 +1,8 @@
 class Homegit < Formula
   desc "Your personal Git server. No cloud, no auth, no complexity."
   homepage "https://github.com/chris-roerig/homegit"
-  url "https://github.com/chris-roerig/homegit/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "0ee7b50940a61065adacf3a76bf669721614d6838261977f359c8d65cf550832"
+  url "https://github.com/chris-roerig/homegit/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "5328181a3996cdaf4850904bbf6d18c9637a0c2babbc3f6eb1ada567b6dbeb39"
   license "MIT"
   head "https://github.com/chris-roerig/homegit.git", branch: "main"
 
